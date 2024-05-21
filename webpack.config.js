798a1b411ebcf40d23df90a5fcb9ad611ca2c60b
Lucycan <lly@icstars.org>
2024-05-21 18:24:02 -0500

    ......

--- webpack.config.js
+++ webpack.config.js
@@ -33,10 +33,10 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "src"),
     },
     compress: true,
-    port: 3000,
+    port: 3001,
     open: true,
     hot: true,
   },
