26ff5a3c6cf82afd3a206f9177baf4caf57383d3
looSeeCan2 <nruabnrabkojthiabkuv@gmail.com>
2024-05-21 23:41:26 -0500

    ....

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Import the plugin
 
 module.exports = {
   mode: "development", // This can be overridden by the --mode option in npm scripts
@@ -8,7 +7,6 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/MilitzaVazquez-AboutMe/", // Replace `<repository-name>` with your GitHub repo name
   },
   module: {
     rules: [
@@ -24,17 +22,13 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"], // Use MiniCssExtractPlugin for CSS
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./docs/index.html", // Ensure this points to your HTML template
-    }),
-    new MiniCssExtractPlugin({
-      // Add the plugin to extract CSS into separate files
-      filename: "styles.css",
     }),
   ],
   devServer: {
